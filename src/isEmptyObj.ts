f50138880ddb63766d4b3482915bb391790e29af
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-08-04 14:13:38 +0300

    добавил проверку типа

--- src/isEmptyObj.ts
+++ src/isEmptyObj.ts
@@ -1,4 +1,8 @@
 export const isEmptyObj = (obj: Object) => {
+  if (typeof obj !== "object") {
+    throw new Error("invalid params");
+  }
+
   for (var prop in obj) {
     if (Object.prototype.hasOwnProperty.call(obj, prop)) {
       return false;
