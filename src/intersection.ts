47200b262bd792ec45ffb27d6ec4e2250927c020
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-08-11 14:30:54 +0300

    add new func - intersection

---
+++ src/intersection.ts
@@ -0,0 +1,16 @@
+export const intersection = (arr1: any[], arr2: any[]) => {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    throw new Error("invalid params");
+  }
+
+  const maxLength = arr1.length >= arr2.length ? arr1.length : arr2.length;
+  const res = [];
+
+  for (let i = 0; i < maxLength; i++) {
+    if (arr1.includes(arr2[i])) {
+      res.push(arr2[i]);
+    }
+  }
+
+  return res;
+};
--- src/minoru.ts
+++ src/minoru.ts
@@ -1,3 +1,4 @@
 export { getUniqueValue } from "./getUniqueValue.js";
 export { isEmptyObj } from "./isEmptyObj.js";
 export { fill } from "./fill.js";
+export { intersection } from "./intersection.js";
